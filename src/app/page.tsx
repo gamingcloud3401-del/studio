ed5b1a3abb0bf1eae6ac90fca223042bd1411bac
Gsss johra Mohra <gsssmohra21@gmail.com>
2025-11-02 06:15:02 +0000

    Mobile pr bhi bina scroll kre 6 or 4 product aa jaye

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
               />
             </div>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-8">
             {isLoading && Array.from({ length: 6 }).map((_, i) => (
                 <div key={i} className="group block">
                     <Card className="h-full overflow-hidden">
